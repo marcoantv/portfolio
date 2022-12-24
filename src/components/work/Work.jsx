fd026b8b14df9d56747e24b0ed3a05a419888edf
Marco Viquez <maviquez1@gmail.com>
2022-12-23 22:48:05 -0600

    worked on the Works section of the portfolio

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,31 +1,64 @@
 import './work.scss'
 
 export default function Work() {
+
+  const data = [
+    {
+      id: "1",
+      icon: "/assets/rocket-launch.png",
+      title: "Web Design",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img:
+        "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+    },
+    {
+      id: "2",
+      icon: "/assets/user.png",
+      title: "Mobile Application",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+    },
+    {
+      id: "3",
+      icon: "/assets/settings.png",
+      title: "Branding",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img:
+        "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+    },
+  ];
   return (
     <div className='work' id='work'>
 
       <div className="slider">
+      
+        {data.map((d) => (
         <div className="container">
           <div className="item">
             <div className="left">
                <div className="leftContainer">
                 <div className="imgContainer">
-                  <img src="assets/rocket-lunch.png" alt="" />
+                  <img src={d.icon} alt="" />
                   
                 </div>
-                <h2>Title</h2>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Commodi sit velit facilis laudantium aliquid voluptates dicta eius tenetur 
-                    rationeidk</p>
+                <h2>{d.title}</h2>
+                  <p>{d.desc}</p> 
                   <span>Projects</span>
                </div>
             </div>
             <div className="right">
-
+                <img src="assets/banking.jpg" alt="" />
             </div>
-          </div>
+          </div>       
         </div>
+        ))}
       </div>
+      <img src="assets/arrow.PNG" className='arrow left' alt="" />
+      <img src="assets/arrow.PNG" className='arrow right' alt="" />
     </div>
   )
 }
